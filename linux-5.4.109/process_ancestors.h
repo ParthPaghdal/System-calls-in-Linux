a1186277e4b0a0c0c8746d650b6f0285218d27d6
Parth Paghdal <84153304+ParthPaghdal@users.noreply.github.com>
2023-05-25 20:35:54 -0700

    Update process_ancestors.h

---
+++ linux-5.4.109/process_ancestors.h
@@ -0,0 +1,23 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/uaccess.h>
+#include <asm/errno.h>
+#include <linux/cred.h>
+
+// Structure to hold values returned by process_ancestors sys-call
+#ifndef _PROCESS_ANCESTORS_H
+#define _PROCESS_ANCESTORS_H
+asmlinkage long sys_process_ancestors(struct process_info *info_array, long size, long *num_filled);
+#define ANCESTOR_NAME_LEN 16
+struct process_info {
+ long pid;                     /* Process ID */
+ char name[ANCESTOR_NAME_LEN]; /* Program name of process */
+ long state;                   /* Current process state */
+ long uid;                     /* User ID of process owner */
+ long nvcsw;                   /* # of voluntary context switches */
+ long nivcsw;                  /* # of involuntary context switches */
+ long num_children;            /* # of children processes */
+ long num_siblings;            /* # of sibling processes */
+};
+
+#endif
